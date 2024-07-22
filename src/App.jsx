49ab6d678d080909ca03f77c5a0e471cc30c4242
evertonlima365 <evertonhihop@gmail.com>
2024-07-22 14:54:34 -0300

    gradiente e logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-
-import React, {useEffect,useState  } from 'react';
-import Lottie from 'react-lottie';
-import './index.css';
-import animation from './components/animações/Animation.json';
-import animation1 from './components/animações/Animation1.json';
-
+import React, { useEffect, useState } from "react";
+import Lottie from "react-lottie";
+import "./index.css";
+import animation from "./components/animações/Animation.json";
+import animation1 from "./components/animações/Animation1.json";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -19,8 +17,8 @@ function App() {
     autoplay: true,
     animationData: animation,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   const defaultOptions1 = {
@@ -28,8 +26,8 @@ function App() {
     autoplay: true,
     animationData: animation1,
     rendererSettings: {
-      preserveAspectRatio: 'xMidYMid slice'
-    }
+      preserveAspectRatio: "xMidYMid slice",
+    },
   };
 
   useEffect(() => {
@@ -37,19 +35,19 @@ function App() {
     function starPosition() {
       return Math.floor(Math.random() * 100) + "%";
     }
-    
+
     // Seleciona todas as estrelas após o componente ser montado
     const stars = document.querySelectorAll(".star");
-    
+
     // Move cada estrela para uma posição aleatória inicial
-    stars.forEach(function(star) {
+    stars.forEach(function (star) {
       star.style.top = starPosition();
       star.style.left = starPosition();
     });
-    
+
     // Função para mover as estrelas aleatoriamente a cada segundo
-    const interval = setInterval(function() {
-      stars.forEach(function(star) {
+    const interval = setInterval(function () {
+      stars.forEach(function (star) {
         star.style.top = starPosition();
         star.style.left = starPosition();
       });
@@ -58,85 +56,139 @@ function App() {
     // Limpa o intervalo quando o componente é desmontado
     return () => clearInterval(interval);
   }, []); // Array vazio para garantir que o useEffect execute apenas uma vez
+
   return (
     <div className="body">
       <header>
-    <section class="profile-section">
-     <img src="./image/Imagem2.jpg" alt="" width="200px" height="200px"/>
-     <div className='digito'>
-    <h1 className='digite'>EVERTON SILVA</h1>
-    
-    <h2 className='digite1'>Desenvolvedor FullStack</h2>
-    
-    </div>
-  
-    <div class="star star1"></div><div class="star star2"></div><div class="star star2"></div>
-    <div class="star star3"></div><div class="star star4"></div><div class="star star5"></div>
-    <div class="star star6"></div><div class="star star3"></div><div class="star star4"></div>
-    <div class="star star5"></div><div class="star star6"></div><div class="star star3"></div>
-    <div class="star star4"></div><div class="star star5"></div><div class="star star6"></div>
-   
-    </section>
-    
-    </header>
-   
+        <div className="title1">
+          <h1>
+            Portifólio.<span>index</span>
+          </h1>
+        </div>
+        <div className="statusbar">
+          <button>Inicio</button>
+          <button>sobre mim</button>
+          <button>serviços</button>
+          <button>Habilidades</button>
+        </div>
+        <section class="profile-section">
+          <img src="./image/Imagem2.jpg" alt="" width="200px" height="200px" />
+          <div className="digito">
+            <h1 className="digite">
+              EVERTON <span> SILVA</span>
+            </h1>
 
-    <main>
-    <section className="info-section">
-      <div>
-      <h2>Bem-vindo à Minha<br/> Página de Projetos</h2>
-      
-      <Lottie
-        options={defaultOptions}
-        height={200}
-        width={200} 
-      />
+            <h2 className="digite1">{"<Desenvolvedor FullStack>"}</h2>
+          </div>
+
+          <div class="star star1"></div>
+          <div class="star star2"></div>
+          <div class="star star2"></div>
+          <div class="star star3"></div>
+          <div class="star star4"></div>
+          <div class="star star5"></div>
+          <div class="star star6"></div>
+          <div class="star star3"></div>
+          <div class="star star4"></div>
+          <div class="star star5"></div>
+          <div class="star star6"></div>
+          <div class="star star3"></div>
+          <div class="star star4"></div>
+          <div class="star star5"></div>
+          <div class="star star6"></div>
+        </section>
+      </header>
+
+      <main>
+        <section className="info-section">
+          <div>
+            <h2>
+              Bem-vindo à Minha
+              <br /> Página de Projetos
+            </h2>
+
+            <Lottie options={defaultOptions} height={200} width={200} />
+          </div>
+          <divi className="text">
+            <p>
+              Eu sou Everton Silva, um profissional da programação com paixão
+              por
+            </p>
+
+            <p>
+              criar soluções inovadoras e eficientes. Nesta página, você
+              encontrará{" "}
+            </p>
+
+            <p>
+              uma coleção de meus projetos mais recentes, abrangendo diversas{" "}
+            </p>
+            <p>áreas da tecnologia e programação.</p>
+            <br />
+            <p>Aplicações web interativas, algoritmos. cada projeto reflete</p>
+            <p> meu compromisso com a excelência e minha dedicação ao</p>
+            <p>
+              desenvolvimento contínuo de habilidades. Navegue pelos projetos{" "}
+            </p>
+            <p>explore o código-fonte e veja como as ideias se transformam </p>
+            <p>em realidade através da programação.</p>
+            <p>Obrigado por visitar minha página de projetos.</p>
+          </divi>
+        </section>
+
+        <h2 id="title">Atividades práticas</h2>
+        <section className="praticas">
+          <div className="link">
+            <h3>Projeto formulários</h3>
+            <div className="text-bnt">
+              <p>Apresento um projeto de</p>
+              <p>formulário em HTML,</p>
+              <p> desenvolvido para explorar</p>
+              <p> conceitos básicos de</p>
+              <p>criação e estruturação de</p>
+              <p> formulários na web.</p>
+            </div>
+            <button>ver projeto</button>
+          </div>
+          <div className="link">
+            <h3>Projeto Receita</h3>
+            <div className="text-bnt">
+              <p>Apresento nosso projeto</p>
+              <p>combinando HTML e CSS, </p>
+              <p>focado em criar um layout </p>
+              <p>responsivo e conceitos</p>
+              <p>básicos de estilizado para</p>
+              <p>demonstrar a integração dessas</p>
+              <p>tecnologias na construção </p>
+              <p>de páginas web.</p>
+            </div>
+            <button>ver projeto</button>
+          </div>
+          <div className="link">
+            <h3>Projeto Tabela</h3>
+            <div className="text-bnt">
+              <p>Apresento nosso projeto de</p>
+              <p>abelas em HTML, desenvolvido</p>
+              <p>para explorar a estruturação e </p>
+              <p>formatação de dados tabulares</p>
+              <p>de maneira clara e organizada</p>
+              <p>na web.</p>
+            </div>
+            <button>ver projeto</button>
+          </div>
+          <div className="animação">
+            <Lottie options={defaultOptions1} height={200} width={200} />
+          </div>
+        </section>
+      </main>
+      <footer>
+        <section className="footer">
+          <divi>
+            <p>&copy; 2024 ewernoute. Todos os direitos reservados.</p>
+          </divi>
+        </section>
+      </footer>
     </div>
-      <divi className="text">
-      <p>Eu sou Everton Silva, um profissional da programação com paixão por</p>
-      
-      <p>criar soluções inovadoras e eficientes. Nesta página, você encontrará </p>
-      
-      <p>uma coleção de meus projetos mais recentes, abrangendo diversas  </p>
-      <p>áreas da tecnologia e programação.</p><br/>
-      <p>Aplicações web interativas, algoritmos. cada projeto reflete</p>
-      <p> meu compromisso com a excelência e minha dedicação ao</p>
-      <p>desenvolvimento contínuo de habilidades. Navegue pelos projetos  </p>
-      <p>explore o código-fonte e veja como as ideias se transformam  </p>
-      <p>em realidade através da programação.</p>
-      <p>Obrigado por visitar minha página de projetos.</p>
-     </divi>
-    
-    </section>
-    
- <h2 id='title'>Atividades práticas</h2>
-    <section className="praticas">
-     
-      <div className='link'>
-      <a href="./src/desafio/desafio.html">Projeto formulários</a>
-      <a href="./src/ex02/ex02.html">Projeto receita</a>
-      <a href="./src/pratica03/pratica3.html">Projeto tabela</a>
-      </div>
-      <div className='animação'>
-      
-      <Lottie
-        options={defaultOptions1}
-        height={200}
-        width={200}
-      />
-    </div>
-      
-    </section>
-    </main>
-    <footer>
-      <section className='footer'>
-        <divi>
-        <p>&copy; 2024 ewernoute. Todos os direitos reservados.</p>
-        </divi>
-      </section>
-    </footer>
-    </div>
-    
   );
 }
 
